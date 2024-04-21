7994f213cb5ba09119afb96de3cb47ad40580f1b
Musya <musya.ilyasova@getorca.app>
2024-04-21 23:54:08 +0300

    add env.js

---
+++ src/js/modules/env.js
@@ -0,0 +1,6 @@
+// prod links
+window.defaultCode = "XYRO1";
+window.campaignID = "661eb6910a3a513613b9ce1b";
+window.apiUrl = "https://staging.grokth.com/api/";
+window.discordLink = "https://discord.com/oauth2/authorize?client_id=1228003487066099845&response_type=code&redirect_uri=https%3A%2F%2Fstaging.grokth.com%2Fen%2Fwallet&scope=identify";
+window.telegram = "https://t.me/GrokthXyroStageBot";
