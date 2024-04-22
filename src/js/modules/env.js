7068ec577ba1ca8af1e1ebae388aa735ec688886
Musya <musya.ilyasova@getorca.app>
2024-04-23 01:13:09 +0300

    fixed urls

--- src/js/modules/env.js
+++ src/js/modules/env.js
@@ -1,6 +1,6 @@
 // prod links
 window.defaultCode = "XYRO1";
 window.campaignID = "661eb6910a3a513613b9ce1b";
-window.apiUrl = "https://staging.grokth.com/api/";
-window.discordLink = "https://discord.com/oauth2/authorize?client_id=1228003487066099845&response_type=code&redirect_uri=https%3A%2F%2Fstaging.grokth.com%2Feng%2Fwallet&scope=identify";
+window.apiUrl = "https://stage.grokth.com/api/";
+window.discordLink = "https://discord.com/oauth2/authorize?client_id=1228003487066099845&response_type=code&redirect_uri=https%3A%2F%2Fstage.grokth.com%2Feng%2Fwallet&scope=identify";
 window.telegram = "https://t.me/GrokthXyroStageBot";
